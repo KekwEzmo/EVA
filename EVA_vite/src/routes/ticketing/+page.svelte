3a930f007ccf4569b3b62312db3bcd5a1959d27e
KekwEzmo <lisice@edu.htl-villach.at>
2024-01-20 00:24:44 +0100

    added search functionality

--- EVA_vite/src/routes/ticketing/+page.svelte
+++ EVA_vite/src/routes/ticketing/+page.svelte
@@ -1,64 +1,64 @@
 <script>
 	import { writable } from 'svelte/store';
-  
+
 	let tableData = writable([]);
-  
+
 	async function fetchData() {
-	  try {
-		const response = await fetch('http://192.168.0.215:5252/getData', {
-  credentials: 'include', // 
-});
-		const { data } = await response.json();
-		console.log('Fetched data:', data);
-		tableData.set(data);
-	  } catch (error) {
-		console.error('Error fetching data:', error);
-	  }
+		try {
+			const response = await fetch('http://192.168.0.215:5252/getData', {
+				credentials: 'include',
+			});
+			const { data } = await response.json();
+			console.log('Fetched data:', data);
+			
+			tableData.set(data.reverse());
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
 	}
-  </script>
-  
-  <section>
+</script>
+
+<section>
 	<button on:click="{fetchData}">Fetch Data</button>
-  
+
 	<table>
-	  <thead>
-		<tr>
-		  <th>ID</th>
-		  <th>User ID</th>
-		  <th>Title</th>
-		  <th>Request</th>
-		  <th>Selected Items</th>
-		</tr>
-	  </thead>
-	  <tbody>
-		{#each $tableData as row (row.id)}
-		  <tr>
-			<td>{row.id}</td>
-			<td>{row.user_id}</td>
-			<td>{row.title}</td>
-			<td>{row.request}</td>
-			<td>{row.selected_items}</td>
-		  </tr>
-		{/each}
-	  </tbody>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>User ID</th>
+				<th>Title</th>
+				<th>Request</th>
+				<th>Selected Items</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each $tableData as row (row.id)}
+				<tr>
+					<td>{row.id}</td>
+					<td>{row.user_id}</td>
+					<td>{row.title}</td>
+					<td>{row.request}</td>
+					<td>{row.selected_items}</td>
+				</tr>
+			{/each}
+		</tbody>
 	</table>
-  </section>
-  
-  <style>
+</section>
+
+<style>
 	table {
-	  width: 100%;
-	  border-collapse: collapse;
-	  margin-top: 20px;
+		width: 100%;
+		border-collapse: collapse;
+		margin-top: 20px;
 	}
-  
+
 	th, td {
-	  border: 1px solid #dddddd;
-	  text-align: left;
-	  padding: 8px;
+		border: 1px solid #dddddd;
+		text-align: left;
+		padding: 8px;
 	}
-  
+
 	th {
-	  background-color: #f2f2f2;
+		background-color: #f2f2f2;
 	}
-  </style>
-  
+</style>
